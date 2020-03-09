549b1b213aefd4c8fb0e90229cdc7b60fd62ca79
khailegend <54508438+khailegend@users.noreply.github.com>
2020-03-10 00:21:00 +0700

    update MenuTop

---
+++ src/components/MenuTop/MenuTop.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+import './MenuTop.css';
+import $ from 'jquery' ;
+import {Link , Route} from 'react-router-dom';
+import Logo from '../../assets/logoResence.png';
+import routes from '../../routes';
+
+const MenuLink = ({ label , to , activeOnlyWhenExact }) => {
+    return (
+        <Route path={to} exact={activeOnlyWhenExact} children={({match}) => {
+            var active= match ? 'active abc' : '';
+            return (
+                <li className={`my-li ${active}`}>
+                    <Link to={to} className="link">
+                        {label}
+                    </Link>
+                </li>
+            )
+        }} />
+    )
+}
+
+class MenuTop extends Component {
+    showMenus = (routes) => {
+        var result = null;
+    
+        if (routes.length > 0) {
+            result = routes.map((route, index) => {
+                return (
+                    <MenuLink 
+                        key={index} 
+                        label={route.name}
+                        to={route.path}
+                        activeOnlyWhenExact={route.exact} 
+                    />
+                );
+            });
+        }
+        return result;
+    }
+    render(){
+        $(document).ready(function(){
+            $('#icon').click(function(){
+               $('ul').toggleClass('show');
+            });
+        });
+
+    return (
+        <div className="MenuTop">
+            <nav className="fixel">
+                <img alt="Logo" id="logo" src={Logo}/>
+                <ul>
+                    {this.showMenus(routes)}
+                </ul>
+                <label id="icon">
+                    <i className="fas fa-bars"></i>
+                </label>
+            </nav>
+        </div>
+        
+        
+    );
+    
+    }
+}
+
+export default MenuTop;
