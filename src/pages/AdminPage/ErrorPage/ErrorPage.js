d3cfbabc805e144669ed3ae7106cc18eeaa4214b
khailegend <54508438+khailegend@users.noreply.github.com>
2020-06-26 00:38:30 +0700

    add Error Page

--- src/pages/AdminPage/ErrorPage/ErrorPage.js
+++ src/pages/AdminPage/ErrorPage/ErrorPage.js
@@ -1,14 +1,19 @@
-import React , { Component } from 'react' ;
+import React, { Component } from 'react';
 
 class ErrorPage extends Component {
-    backPageBefore(){
+    backPageBefore() {
         window.history.back();
+       
     }
-    render(){
-        return(
-            <div>
-                <h1>ERROR PAGE</h1>
-                <button onClick = {this.backPageBefore} type='button'> back</button>
+    render() {
+        return (
+            <div className='errorPage' style={{backgroundColor: '#34495e' , paddingTop : '10%', paddingBottom : '14%'}}>
+                <div className='text-center'>
+                    <h1 style={{ fontSize: '800%', color: 'white' }}>404</h1>
+                    <h1 style={{ fontSize: '500%', color: 'white' }}>ERROR</h1>
+                    <h3 style={{ color: 'white' }}>Page not found :(</h3>
+                    <p  style={{ color: 'white' }} className="btn" onClick={this.backPageBefore} > Return to the Default Page </p>
+                </div>
             </div>
         )
     }
